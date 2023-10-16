4eafc9d06436efce2c8ac33591ae83680d42d329
Filipe Guidastri <117661407+FiGuidastri@users.noreply.github.com>
2023-10-16 14:57:18 -0300

    Update consulta_meio_pgto.sql
    Adicionada variável e agrupamento

--- consulta_meio_pgto.sql
+++ consulta_meio_pgto.sql
@@ -1,12 +1,14 @@
+DECLARE @DataAtual DATE = CAST(GETDATE() AS DATE);
+
+-- Primeira parte da consulta
 SELECT CONVERT(VARCHAR(10), t.DataOcorrencia, 103) AS DataPassagem,
-	t.CategoriaClassificada AS Categoria,
-	t.EixosClassificados AS Eixos,  
-	SUM(t.TotalVeiculo) AS Transacoes,
-	t.ValorNominal,
-	t.Cobranca,
-	SUBSTRING(CONCAT(o.Descricao, ''), 4, 5) AS Sentido,
-	o.Praca AS IdPraca,
-	L.DESCRICAO AS MeioPGTO
+    o.Praca AS IdPraca,
+    t.CategoriaClassificada AS Categoria,
+    L.DESCRICAO AS MeioPGTO,
+    SUM(t.TotalVeiculo) AS Transacoes,
+    t.ValorNominal,
+    t.Cobranca,
+    SUBSTRING(CONCAT(o.Descricao, ''), 4, 5) AS Sentido
 FROM Transacao T
 LEFT JOIN Origem O ON T.IdOrigem = O.IdOrigem
 LEFT JOIN CodigoMensagem CM ON T.CodigoCV = CM.CodigoCV
@@ -14,30 +16,28 @@ LEFT JOIN TransacaoDetalhe td ON td.IdTransacao = T.IdTransacao
 LEFT JOIN MEIOPAGAMENTO L ON TD.IdMeioPagamento = L.IdMeioPagamento
 INNER JOIN SgCfgCobrancaMeioPagamento CB ON CB.IdCobranca = T.Cobranca
 WHERE 
-	t.Cobranca IN ('1', '6') 
-	AND CAST(t.DataOcorrencia AS DATE) BETWEEN '2023-09-19' AND '2023-09-19'
+    t.Cobranca IN ('1', '6') 
+    AND CAST(t.DataOcorrencia AS DATE) BETWEEN '2023-01-01' AND DATEADD(DAY, -1, @DataAtual)
 GROUP BY
-	CONVERT(VARCHAR(10), t.DataOcorrencia, 103),
-	t.CategoriaClassificada,
-	t.EixosClassificados,
-	L.DESCRICAO,
-	t.ValorNominal,
-	t.Cobranca,
-	SUBSTRING(CONCAT(o.Descricao, ''), 4, 5),
-	o.Descricao,
-	o.Praca
+    CONVERT(VARCHAR(10), t.DataOcorrencia, 103),
+    o.Praca,
+    t.CategoriaClassificada,
+    L.DESCRICAO,
+    t.ValorNominal,
+    t.Cobranca,
+    SUBSTRING(CONCAT(o.Descricao, ''), 4, 5)
 
 UNION ALL
 
+-- Segunda parte da consulta
 SELECT CONVERT(VARCHAR(10), t.DataOcorrencia, 103) AS DataPassagem,
-	t.CategoriaClassificada AS Categoria,
-	EixosCorrigidos AS Eixos,  
-	SUM(t.TotalVeiculo) AS Transacoes,
-	SUM(t.ValorNominalCorrigido) AS ValorNominalCorrigido,
-	t.Cobranca,
-	SUBSTRING(CONCAT(o.Descricao, ''), 4, 5) AS Sentido,
-	o.Praca AS IdPraca,
-	L.DESCRICAO AS MeioPGTO
+    o.Praca AS IdPraca,
+    t.CategoriaClassificada AS Categoria,
+    L.DESCRICAO AS MeioPGTO,
+    SUM(t.TotalVeiculo) AS Transacoes,
+    SUM(t.ValorNominalCorrigido) AS ValorNominalCorrigido,
+    t.Cobranca,
+    SUBSTRING(CONCAT(o.Descricao, ''), 4, 5) AS Sentido
 FROM CorrecaoTransacao T
 LEFT JOIN Origem O ON T.IdOrigem = O.IdOrigem
 LEFT JOIN CodigoMensagem CM ON T.CodigoCV = CM.CodigoCV
@@ -45,15 +45,13 @@ LEFT JOIN CorrecaoTransacaoDetalhe td ON td.IdTransacao = T.IdTransacao
 LEFT JOIN MEIOPAGAMENTO L ON TD.IdMeioPagamento = L.IdMeioPagamento
 INNER JOIN SgCfgCobrancaMeioPagamento CB ON CB.IdCobranca = T.Cobranca
 WHERE 
-	t.Cobranca IN ('1', '3', '6') 
-	AND t.IdAcaoCorrecao <> '4'
-	AND CAST(t.DataOcorrencia AS DATE) BETWEEN '2023-09-19' AND '2023-09-19'
+    t.Cobranca IN ('1', '3', '6') 
+    AND t.IdAcaoCorrecao <> '4'
+    AND CAST(t.DataOcorrencia AS DATE) BETWEEN '2023-01-01' AND DATEADD(DAY, -1, @DataAtual)
 GROUP BY
-	CONVERT(VARCHAR(10), t.DataOcorrencia, 103),
-	t.CategoriaClassificada,
-	EixosCorrigidos,
-	L.DESCRICAO,
-	t.Cobranca,
-	SUBSTRING(CONCAT(o.Descricao, ''), 4, 5),
-	o.Descricao,
-	o.Praca
+    CONVERT(VARCHAR(10), t.DataOcorrencia, 103),
+    o.Praca,
+    t.CategoriaClassificada,
+    L.DESCRICAO,
+    t.Cobranca,
+    SUBSTRING(CONCAT(o.Descricao, ''), 4, 5)
